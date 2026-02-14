e15906130899d80b9f97182972ef3fa89ada4f95
Xander <XanderG2@users.noreply.github.com>
2026-02-14 22:35:52 +0000

    change to camel case

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def clear() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def clearLine(n: int = 1) -> None:
+def clear_line(n: int = 1) -> None:
     """
     Clear the last `n` lines from the console
 
@@ -32,16 +32,16 @@ def clearLine(n: int = 1) -> None:
     print("\033[1A\033[2K\r"*n, end="")
 
 
-def getWords(filePath: str) -> list[str]:
+def get_words(file_path: str) -> list[str]:
     """
     Read words from word file
 
-    :param filePath: Word file location
-    :type filePath: str
+    :param file_path: Word file location
+    :type file_path: str
     :return: List of words read from word file
     :rtype: list[str]
     """
-    with open(filePath, "r") as file:
+    with open(file_path, "r") as file:
         words: list[str] = [word.strip() for word in file]
 
     if len(words) == 0:
@@ -50,26 +50,26 @@ def getWords(filePath: str) -> list[str]:
     return words
 
 
-def getWordFilePath(defaultFilePath: str) -> str:
+def get_word_file_path(default_file_path: str) -> str:
     """
-    Get the filepath of the words file
+    Get the file_path of the words file
 
-    :param defaultFilePath: The default file path this should assume
+    :param default_file_path: The default file path this should assume
         exists, if it doesnt exist, it will ask the user to select a
         different .txt file.
-    :type defaultFilePath: str
-    :return: Filepath of words file
+    :type default_file_path: str
+    :return: file_path of words file
     :rtype: str
     """
-    if os.path.exists(defaultFilePath):
-        return defaultFilePath
+    if os.path.exists(default_file_path):
+        return default_file_path
 
     print(f"No word file found. The default words file name is {WORD_FILE}")
 
     path = "."
     files = [f for f in os.listdir(path)
-             if os.path.isfile(os.path.join(path, f))
-             and f.endswith(".txt")
+             if f.endswith(".txt")
+             and os.path.isfile(os.path.join(path, f))
              ]
 
     print("All text files found in current directory:")
@@ -91,20 +91,21 @@ def getWordFilePath(defaultFilePath: str) -> str:
     return files[file]
 
 
-def requestLetters(
-        maxLetters: int,
-        minLetters: int,
+def request_letters(
+        max_letters: int,
+        min_letters: int,
         words: list[str]
 ) -> tuple[int, list[str], bool]:
     """
     Ask user for amount of letters in word, will also determin dev mode
       on/off
 
-    :param maxLetters: The length of the longest word in the words file
-    :type maxLetters: int
-    :param minLetters: The length of the shortest word in the words
+    :param max_letters: The length of the longest word in the words
       file
-    :type minLetters: int
+    :type max_letters: int
+    :param min_letters: The length of the shortest word in the words
+      file
+    :type min_letters: int
     :param words: The words in the words file
     :type words: list[str]
     :return: Amount of letters in word, valid words list, developer
@@ -112,46 +113,44 @@ def requestLetters(
     :rtype: tuple[int, list[str], bool]
     """
     while True:
-        lettersInput: str = input(
+        letters_input: str = input(
             "Please enter the amount of letters in the word you would like "
-            f"to guess ({minLetters}-{maxLetters}):\n> "
+            f"to guess ({min_letters}-{max_letters}):\n> "
         )
-        if lettersInput != "dev":
-            if lettersInput.isdigit():
-                letters: int = int(lettersInput)
-
-                if minLetters > letters or maxLetters < letters:
-                    print(
-                        f"Please enter a value between {minLetters} and "
-                        f"{maxLetters}."
-                    )
-                    continue
-
-                validWords: list[str] = [x.strip()
-                                         for x in words
-                                         if len(x.strip()) == letters]
-
-                if len(validWords) == 0:
-                    print(f"There are no words {letters} letters long")
-                    continue
-
-                return letters, validWords, False
-
-            else:
-                print("Please put a number.")
-
-        else:
+        if letters_input == "dev":
             print("Dev mode activated.")
             letters: int = int(input(
-                "Please enter the amount of letters in the word you would" +
+                "Please enter the amount of letters in the word you would"
                 " like to guess:\n> "
             ))
             return letters, [], True
 
+        if not letters_input.isdigit():
+            print("Please put a number.")
+            continue
 
-def validateGuess(
+        letters: int = int(letters_input)
+
+        if letters < min_letters or letters > max_letters:
+            print(
+                f"Please enter a value between {min_letters} and "
+                f"{max_letters}."
+            )
+            continue
+
+        valid_words: list[str] = [x for x in words
+                                  if len(x) == letters]
+
+        if len(valid_words) == 0:
+            print(f"There are no words {letters} letters long")
+            continue
+
+        return letters, valid_words, False
+
+
+def validate_guess(
         letters: int,
-        validWords: list[str],
+        valid_words: list[str],
         dev: bool
 ) -> tuple[str, bool]:
     """
@@ -161,15 +160,15 @@ def validateGuess(
     :param letters: The amount of letters in the word the user is
       trying to guess
     :type letters: int
-    :param validWords: The guessable words
-    :type validWords: list[str]
+    :param valid_words: The guessable words
+    :type valid_words: list[str]
     :param dev: Whether the user is in dev mode or not
     :type dev: bool
     :return: Guess, and give up status
     :rtype: tuple[str, bool]
     """
     while True:
-        guess: str = input().strip()
+        guess: str = input().strip().lower()
 
         if guess == "giveup":
             return guess, True
@@ -177,19 +176,19 @@ def validateGuess(
         if len(guess) != letters:
             print("Please guess the correct amount of letters!")
             time.sleep(0.75)
-            clearLine(2)
+            clear_line(2)
             continue
 
-        if guess not in validWords and not dev:
+        if guess not in valid_words and not dev:
             print("Please guess an existing word!")
             time.sleep(0.75)
-            clearLine(2)
+            clear_line(2)
             continue
 
         return guess, False
 
 
-def formatLetter(result: str, letter: str) -> str:
+def format_letter(result: str, letter: str) -> str:
     """
     Format a letter with background colour according to the
     correct/wrong/place indicator
@@ -209,7 +208,7 @@ def play(
         colors: bool,
         letters: int,
         word: str,
-        validWords: list[str],
+        valid_words: list[str],
         dev: bool
 ) -> int:
     """
@@ -225,14 +224,14 @@ def play(
     :type letters: int
     :param word: The word the user is trying to guess
     :type word: str
-    :param validWords: The guessable words
-    :type validWords: list[str]
+    :param valid_words: The guessable words
+    :type valid_words: list[str]
     :param dev: Whether the user is in dev mode or not
     :type dev: bool
     :return: Status code
     :rtype: int
     """
-    guess, giveup = validateGuess(letters, validWords, dev)
+    guess, giveup = validate_guess(letters, valid_words, dev)
 
     if giveup:
         return 1
@@ -241,7 +240,7 @@ def play(
     corrects: int = 0
     # useful for yellow marking
     # if word has only 1 of a letter only mark it once, etc.
-    numOfLetters: dict[str, int] = {
+    num_of_letters: dict[str, int] = {
         letter: word.count(letter) for letter in set(word)
     }
 
@@ -249,22 +248,22 @@ def play(
         if word[i] == letter:
             truth[i] = "C"
             corrects += 1
-            numOfLetters[letter] -= 1
+            num_of_letters[letter] -= 1
 
     for i, letter in enumerate(guess):
-        if (letter in word and numOfLetters[letter] > 0 and
+        if (letter in word and num_of_letters[letter] > 0 and
                 not word[i] == letter):
             truth[i] = "W"
-            numOfLetters[letter] -= 1
+            num_of_letters[letter] -= 1
         elif word[i] != letter:
             truth[i] = "X"
 
     if colors:
         for i, letter in enumerate(guess):
             result = truth[i]
-            truth[i] = formatLetter(result, letter)
+            truth[i] = format_letter(result, letter)
 
-        clearLine()
+        clear_line()
 
     print("".join(truth))
 
@@ -275,12 +274,12 @@ def play(
 
 
 def main():
-    filePath: str = getWordFilePath(WORD_FILE)
-    words: list[str] = getWords(filePath)
+    file_path: str = get_word_file_path(WORD_FILE)
+    words: list[str] = get_words(file_path)
 
-    maxLetters: int = max(len(word) for word in words)
-    minLetters: int = min(len(word) for word in words)
-    minLetters = 3 if maxLetters > 3 else minLetters
+    max_letters: int = max(len(word) for word in words)
+    min_letters: int = min(len(word) for word in words)
+    min_letters = 3 if max_letters > 3 else min_letters
 
     colors: bool = input(CORRECT + "Can you see the background color? (y/n)" +
                          RESET + "\n> ").lower() in ["y", "yes"]
@@ -288,8 +287,8 @@ def main():
     clear()
     playing = True
     while playing:
-        letters, validWords, dev = requestLetters(
-            maxLetters, minLetters, words)
+        letters, valid_words, dev = request_letters(
+            max_letters, min_letters, words)
 
         clear()
         print("--------- Python Wordle ---------")
@@ -301,12 +300,12 @@ def main():
             print("C = Correct place   W = Wrong place   X = Not in word")
         print("\n")
 
-        word: str = input("Word: ") if dev else random.choice(validWords)
+        word: str = input("Word: ") if dev else random.choice(valid_words)
         giveup: bool = False
         status: int = 0
 
         while not status:
-            status = play(colors, letters, word, validWords, dev)
+            status = play(colors, letters, word, valid_words, dev)
             giveup = status == 1
 
         if giveup:
